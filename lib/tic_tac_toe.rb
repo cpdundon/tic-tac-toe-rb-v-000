4a5f7e57e4a58424194c9bd62813b4fc93258132
Chris Dundon <chris.dundon@gmail.com>
2018-03-27 00:52:13 +0000

    automatically backed up by learn

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -1,1 +1,48 @@
+WIN_COMBINATIONS = [
+  [0, 1, 2],
+  [3, 4, 5],
+  [6, 7, 8],
+  [0, 3, 6],
+  [1, 4, 7],
+  [2, 5, 8],
+  [0, 4, 8],
+  [2, 4, 6]
+  ]
 
+def display_board(board)
+  puts " #{board[0]} | #{board[1]} | #{board[2]} "
+  puts "-----------"
+  puts " #{board[3]} | #{board[4]} | #{board[5]} "
+  puts "-----------"
+  puts " #{board[6]} | #{board[7]} | #{board[8]} "
+end
+
+def input_to_index(input)
+  return (input.to_i - 1)
+end
+
+def move(board, idx, token = "X")
+  #idx = input_to_index(input)
+  
+  if valid_move?(board, idx)
+    board[idx] = token
+  end
+  
+  display_board(board)
+end
+
+def valid_move?(board, idx)
+  if !idx.between(0, 8)
+    return false
+  end
+  
+  !position_taken?(board, idx)
+end
+
+def position_taken?(board, idx)
+  chr = board[idx]
+  if chr == " "
+    return false
+  end
+  true
+end
